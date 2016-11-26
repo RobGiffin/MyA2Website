4bf8d879224028ad12d92990b520decb98a4aae7
RobGiffin <giffinr@googlemail.com>
2016-11-26 10:49:44 +0000

    first full commit

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+// moduleId:module.id and templateURL is used to load the contents from a separate html file.
+
+@Component({
+  moduleId:module.id,
+  selector: 'my-app',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent { }
